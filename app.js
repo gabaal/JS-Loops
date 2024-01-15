077b9a13ee51183f292d29d62b43ed96d91083a1
gabaal <geraldbaalham@live.co.uk>
2024-01-15 12:01:58 +0000

    Initial files created

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log("Log something to check its working");
+const blogPosts = ["1st blog", "2nd blog", "3rd blog"];
+
+console.log(blogPosts);
+
+console.log(blogPosts[0]); //Output :1st blog
+console.log(blogPosts[1]); //Output :2nd blog
+console.log(blogPosts[2]); //Output :3rd blog
+
+//Adding Items
+
+blogPosts[3] = "4th blog";
+console.log(blogPosts);
+console.log(blogPosts[3]); //Output :4th blog
+
+// FavouriteFoods Array
+
+const favouriteFoods = ["Banana", "Apple", "Toad in the Hole"];
+console.log(favouriteFoods);
+
+// FavouriteColours Array and add a element
+
+const favouriteColours = ["red", "blue", "green"];
+console.log(favouriteColours);
+favouriteColours[3] = "green";
+console.log(favouriteColours);
+
+// FavouriteNumbers and push command
+
+const favouriteNumbers = [13, 28, 6, 7];
+console.log(favouriteNumbers);
+favouriteNumbers.push(1972);
+console.log(favouriteNumbers);
+favouriteNumbers.push(1980);
+console.log(favouriteNumbers);
