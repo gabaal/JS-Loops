c312207cb3381ce13b0bc98bbb08f615b5bba902
gabaal <geraldbaalham@live.co.uk>
2024-01-15 12:34:01 +0000

    JS Loops lesson finished

--- app.js
+++ app.js
@@ -1,35 +1,66 @@
 console.log("Log something to check its working");
-const blogPosts = ["1st blog", "2nd blog", "3rd blog"];
 
-console.log(blogPosts);
+// FavouriteNumbers Array and push command
 
-console.log(blogPosts[0]); //Output :1st blog
-console.log(blogPosts[1]); //Output :2nd blog
-console.log(blogPosts[2]); //Output :3rd blog
-
-//Adding Items
-
-blogPosts[3] = "4th blog";
-console.log(blogPosts);
-console.log(blogPosts[3]); //Output :4th blog
-
-// FavouriteFoods Array
-
-const favouriteFoods = ["Banana", "Apple", "Toad in the Hole"];
-console.log(favouriteFoods);
-
-// FavouriteColours Array and add a element
-
-const favouriteColours = ["red", "blue", "green"];
-console.log(favouriteColours);
-favouriteColours[3] = "green";
-console.log(favouriteColours);
-
-// FavouriteNumbers and push command
-
-const favouriteNumbers = [13, 28, 6, 7];
+const favouriteNumbers = [13, 28, 6, 7, Infinity];
 console.log(favouriteNumbers);
 favouriteNumbers.push(1972);
 console.log(favouriteNumbers);
 favouriteNumbers.push(1980);
 console.log(favouriteNumbers);
+
+// For loops
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration", i);
+}
+
+// While loops
+let counter = 1;
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+
+//While with random number until number < 0.1
+
+let keepLooping = true;
+let i = 0;
+while (keepLooping === true) {
+  console.log("Iteration", i);
+  // set keepLooping to false with 10% chance: Math.random() returns a random value between 0 and 1
+  keepLooping = Math.random() < 0.2;
+  i++;
+}
+
+//Looping over Arrays
+
+const foods = ["pizza", "chocolate", "ice cream", "cheese"];
+for (let i = 0; i < foods.length; i++) {
+  console.log("Loop using .length" + foods[i]);
+}
+// Alternative method of looping
+for (let food of foods) {
+  console.log("Loop without using .length, tidier. " + food);
+}
+
+for (let number of favouriteNumbers) {
+  console.log(number);
+}
+
+//Looping over Arrays forEach method
+
+foods.forEach(function (food) {
+  console.log("forEach loop: " + food);
+});
+
+//Workshop Activities
+
+const favouriteAnimals = ["dog", "cat", "fish", "birds", "elephants"];
+//Method 1 forEach
+favouriteAnimals.forEach(function (animal) {
+  console.log("forEach:" + animal);
+});
+//Method 2 for
+for (let animal of favouriteAnimals) {
+  console.log("for with let " + animal);
+}
